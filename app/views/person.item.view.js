ad1cab6be4641bb7cae9a0607e01fc29d39804cd
Matías M. Méndez <matimendez88@gmail.com>
2015-10-11 19:20:16 -0300

    person events added

--- app/views/person.item.view.js
+++ app/views/person.item.view.js
@@ -10,7 +10,59 @@ App.module('Vamo.Views', function (Views, App, Backbone, Marionette, $, _) {
 
         template: __templates.vamo.person,
 
-        initialize: function() {}
+        ui: {
+            money: 'input[type="number"]',
+            name: 'input[type="text"]',
+            trash: '.icon-remove'
+        },
+
+        events: {
+            'input @ui.money': 'moneyChange',
+            'input @ui.name': 'nameChange',
+            'click @ui.trash': 'removePerson'
+        },
+
+        initialize: function() {},
+
+        moneyChange: function() {
+            var $moneyInput = this.$el.find('input[type="number"]'),
+                newMoneyValue = $moneyInput.val();
+
+            if ( /^[0-9]+$/.test(newMoneyValue) ) {
+                // Si es un número, lo guardo
+                this.model.set({
+                    'money': newMoneyValue
+                });
+            } else {
+                // Si el input está vacio, guardo 0
+                if ($moneyInput.val() === "") {
+                    this.model.set({
+                        'money': 0
+                    });
+                } else {
+                    // Si no es un número, dejo que el valor del input sea el último guardado
+                    $moneyInput.val(this.model.get('money'));
+                }
+            }
+        },
+
+        nameChange: function() {
+            this.model.set({
+                'name': this.$el.find('input[type="text"]').val()
+            });
+        },
+
+        removePerson: function() {
+            var that = this;
+
+            console.log("removePerson");
+
+            this.model.destroy({
+                success: function() {
+                    that.remove();
+                }
+            });
+        }
 
     });
 });
